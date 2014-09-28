afab854a78e4923dab1b4e7020b418e8e719df10
Andre Winkler <gluehloch@gmail.com>
2014-09-28 06:36:39 +0200

    UPDATE: bo_season gets some new columns for openligadb.

--- src/database/update-2.0.0/update_bo_season.sql
+++ src/database/update-2.0.0/update_bo_season.sql
@@ -4,6 +4,8 @@ select version();
 -- Removal of bo_season#bo_current_ref
 ALTER TABLE bo_season DROP FOREIGN KEY fk_season_gamelist;
 ALTER TABLE bo_season DROP bo_current_ref;
+ALTER TABLE bo_season ADD bo_openligaleagueshortcut VARCHAR(20) NULL DEFAULT NULL COMMENT 'Openligadb league shortcut';
+ALTER TABLE bo_season ADD bo_openligaleagueseason VARCHAR(20) NULL DEFAULT NULL COMMENT 'Openligadb league season';
 
 -- Reference to openligadb team id
 ALTER TABLE bo_team ADD bo_openligaid BIGINT NULL DEFAULT NULL COMMENT 'Openligadb team ID' , ADD UNIQUE (bo_openligaid);
--- src/main/java/de/winkler/betoffice/storage/ChampionshipConfiguration.java
+++ src/main/java/de/winkler/betoffice/storage/ChampionshipConfiguration.java
@@ -1,42 +1,65 @@
 /*
- * $Id: ChampionshipConfiguration.java 3782 2013-07-27 08:44:32Z andrewinkler $
  * ============================================================================
- * Project betoffice-storage
- * Copyright (c) 2000-2010 by Andre Winkler. All rights reserved.
+ * Project betoffice-storage Copyright (c) 2000-2010 by Andre Winkler. All
+ * rights reserved.
  * ============================================================================
- *          GNU GENERAL PUBLIC LICENSE
- *  TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION
- *
- *   This program is free software; you can redistribute it and/or modify
- *   it under the terms of the GNU General Public License as published by
- *   the Free Software Foundation; either version 2 of the License, or
- *   (at your option) any later version.
- *
- *   This program is distributed in the hope that it will be useful,
- *   but WITHOUT ANY WARRANTY; without even the implied warranty of
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *   GNU General Public License for more details.
- *
- *   You should have received a copy of the GNU General Public License
- *   along with this program; if not, write to the Free Software
- *   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- *
+ * GNU GENERAL PUBLIC LICENSE TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND
+ * MODIFICATION
+ * 
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the License, or (at your option) any later
+ * version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc., 59 Temple
+ * Place, Suite 330, Boston, MA 02111-1307 USA
  */
 
 package de.winkler.betoffice.storage;
 
 /**
  * Verwaltet die administrativen Daten einer Meisterschaft. Zum Beispiel die
- * Info, in welches Verzeichnis ein Export geht oder welches Export-Templates
- * zu verwenden sind.
+ * Info, in welches Verzeichnis ein Export geht oder welches Export-Templates zu
+ * verwenden sind.
  *
- * @author  $Author: andrewinkler $
- * @version $Revision: 3782 $ $Date: 2013-07-27 10:44:32 +0200 (Sat, 27 Jul 2013) $
+ * @author Andre Winkler
  */
 public class ChampionshipConfiguration extends AbstractStorageObject {
 
     /** serial version id */
     private static final long serialVersionUID = 8221108184744478989L;
+
+    // -- openligadb leagueShortcut -------------------------------------------
+
+    /** The openligadb league shortcut ('bl1' for 'Fussball Bundesliga'). */
+    private String openligaLeagueShortcut;
+
+    public String getOpenligaLeagueShortcut() {
+        return openligaLeagueShortcut;
+    }
+
+    public void setOpenligaLeagueShortcut(String value) {
+        openligaLeagueShortcut = value;
+    }
+
+    // -- openligadb leagueSeason ---------------------------------------------
+
+    /** The openligadb league-season value (Something like '2014'). */
+    private String openligaLeagueSeason;
+
+    public String getOpenligaLeagueSeason() {
+        return openligaLeagueSeason;
+    }
+
+    public void setOpenligaLeagueSeason(String value) {
+        openligaLeagueSeason = value;
+    }
 
     // -- exportDirectory -----------------------------------------------------
 
@@ -60,7 +83,8 @@ public class ChampionshipConfiguration extends AbstractStorageObject {
     /**
      * Setzt ein neues Exportverzeichnis.
      *
-     * @param value Das Exportverzeichnis.
+     * @param value
+     *            Das Exportverzeichnis.
      */
     public void setExportDirectory(final String value) {
         exportDirectory = value;
@@ -85,7 +109,8 @@ public class ChampionshipConfiguration extends AbstractStorageObject {
     /**
      * Setzt ein neues Export-Template.
      *
-     * @param value Das Export-Template.
+     * @param value
+     *            Das Export-Template.
      */
     public void setExportTemplate(final String value) {
         exportTemplate = value;
