9d14404f7264e2c609269bf34ccac5480a1d92b8
Andre Winkler <gluehloch@googlemail.com>
2016-06-10 15:47:54 +0200

    UPDATE: Database script for the EM 2016.

--- src/database/em2016.sql
+++ src/database/em2016.sql
@@ -1,5 +1,5 @@
 -- EM 2016
-
+START TRANSACTION;
 SELECT 'The new teams ...' AS '';
 
 -- Die neuen Mannschaften
@@ -136,78 +136,111 @@ INSERT INTO bo_location(id, bo_name, bo_city, bo_geodat, bo_openligaid)
 
 SELECT 'The new EM 2016 rounds ...' AS '';
 
+/*
 -- Spieltag (688 Start-ID).
 -- Fr 	10.06. 21:00 	Paris St. Denis 	A 	Frankreich 	 -  	Rumänien
+*/
 INSERT INTO bo_gamelist(id, bo_index, bo_date, bo_season_ref, bo_group_ref , bo_datetime, bo_openligaid)
-    VALUES(688, 0, '2016-06-10 21:00', 24, 90, NULL, NULL);
+    VALUES(688, 0, '2016-06-10 21:00', 24, 90, '2016-06-10 21:00', NULL);
 
+/*
+INSERT INTO bo_game(id, bo_date, bo_group_ref, bo_hometeam_ref, bo_guestteam_ref, bo_homegoals, bo_guestgoals, bo_isplayed, bo_gamelist_ref, bo_index, bo_datetime)
+    VALUES(5300, NULL, 90, 688, ...
+*/
+    
+/*
 --Sa 	11.06. 15:00 	Lens 	A 	Albanien 	 -  	Schweiz
 --Sa 	11.06. 18:00 	Bordeaux 	B 	Wales 	 -  	Slowakei
 --Sa 	11.06. 21:00 	Marseille 	B 	England 	 -  	Russland
+*/
 INSERT INTO bo_gamelist(id, bo_index, bo_date, bo_season_ref, bo_group_ref , bo_datetime, bo_openligaid)
-    VALUES(689, 1, '2016-06-11 15:00', 24, 90, NULL, NULL);
+    VALUES(689, 1, '2016-06-11 15:00', 24, 90, '2016-06-11 15:00', NULL);
 
+/*
 --So 	12.06. 15:00 	Paris 	D 	Türkei 	 -  	Kroatien
 --So 	12.06. 18:00 	Nizza 	C 	Polen 	 -  	Nordirland
 --So 	12.06. 21:00 	Lille 	C 	Deutschland 	 -  	Ukraine
+*/
 INSERT INTO bo_gamelist(id, bo_index, bo_date, bo_season_ref, bo_group_ref , bo_datetime, bo_openligaid)
-    VALUES(690, 2, '2016-06-12 15:00', 24, 93, NULL, NULL);
+    VALUES(690, 2, '2016-06-12 15:00', 24, 93, '2016-06-12 15:00', NULL);
 
+/*
 --Mo 	13.06. 15:00 	Toulouse 	D 	Spanien 	 -  	Tschechien
 --Mo 	13.06. 18:00 	Paris St. Denis 	E 	Irland 	 -  	Schweden
 --Mo 	13.06. 21:00 	Lyon 	E 	Belgien 	 -  	Italien
+*/
 INSERT INTO bo_gamelist(id, bo_index, bo_date, bo_season_ref, bo_group_ref , bo_datetime, bo_openligaid)
-    VALUES(691, 3, '2016-06-13 15:00', 24, 93, NULL, NULL);
+    VALUES(691, 3, '2016-06-13 15:00', 24, 93, '2016-06-13 15:00', NULL);
 
+/*
 --Di 	14.06. 18:00 	Bordeaux 	F 	Österreich 	 -  	Ungarn
 --Di 	14.06. 21:00 	Saint Étienne 	F 	Portugal 	 -  	Island
+*/
 INSERT INTO bo_gamelist(id, bo_index, bo_date, bo_season_ref, bo_group_ref , bo_datetime, bo_openligaid)
-    VALUES(692, 4, '2016-06-14 18:00', 24, 94, NULL, NULL);
+    VALUES(692, 4, '2016-06-14 18:00', 24, 94,  '2016-06-14 18:00', NULL);
 
+/*
 --Mi 	15.06. 15:00 	Lille 	B 	Russland 	 -  	Slowakei
 --Mi 	15.06. 18:00 	Paris 	A 	Rumänien 	 -  	Schweiz
 --Mi 	15.06. 21:00 	Marseille 	A 	Frankreich 	 -  	Albanien
+*/
 INSERT INTO bo_gamelist(id, bo_index, bo_date, bo_season_ref, bo_group_ref , bo_datetime, bo_openligaid)
-    VALUES(693, 5, '2016-06-15 15:00', 24, 94, NULL, NULL);
+    VALUES(693, 5, '2016-06-15 15:00', 24, 94, '2016-06-15 15:00', NULL);
 
+/*
 --Do 	16.06. 15:00 	Lens 	B 	England 	 -  	Wales
 --Do 	16.06. 18:00 	Lyon 	C 	Ukraine 	 -  	Nordirland
 --Do 	16.06. 21:00 	Paris St. Denis 	C 	Deutschland 	 -  	Polen
+*/
 INSERT INTO bo_gamelist(id, bo_index, bo_date, bo_season_ref, bo_group_ref , bo_datetime, bo_openligaid)
-    VALUES(694, 6, '2016-06-16 15:00', 24, 91, NULL, NULL);
+    VALUES(694, 6, '2016-06-16 15:00', 24, 91, '2016-06-16 15:00', NULL);
 
+/*
 --Fr 	17.06. 15:00 	Toulouse 	E 	Italien 	 -  	Schweden
 --Fr 	17.06. 18:00 	Saint Étienne 	D 	Tschechien 	 -  	Kroatien
 --Fr 	17.06. 21:00 	Nizza 	D 	Spanien 	 -  	Türkei
+*/
 INSERT INTO bo_gamelist(id, bo_index, bo_date, bo_season_ref, bo_group_ref , bo_datetime, bo_openligaid)
-    VALUES(695, 7, '2016-06-17 15:00', 24, 94, NULL, NULL);
+    VALUES(695, 7, '2016-06-17 15:00', 24, 94, '2016-06-17 15:00', NULL);
 
+/*
 --Sa 	18.06. 15:00 	Bordeaux 	E 	Belgien 	 -  	Irland
 --Sa 	18.06. 18:00 	Marseille 	F 	Island 	 -  	Ungarn
 --Sa 	18.06. 21:00 	Paris 	F 	Portugal 	 -  	Österreich
+*/
 INSERT INTO bo_gamelist(id, bo_index, bo_date, bo_season_ref, bo_group_ref , bo_datetime, bo_openligaid)
-    VALUES(696, 8, '2016-06-18 15:00', 24, 94, NULL, NULL);
+    VALUES(696, 8, '2016-06-18 15:00', 24, 94, '2016-06-18 15:00', NULL);
 
+/*
 --So 	19.06. 21:00 	Lille 	A 	Schweiz 	 -  	Frankreich
 --So 	19.06. 21:00  	Lyon 	A 	Rumänien 	 -  	Albanien
+*/
 INSERT INTO bo_gamelist(id, bo_index, bo_date, bo_season_ref, bo_group_ref , bo_datetime, bo_openligaid)
-    VALUES(697, 9, '2016-06-18 15:00', 24, 90, NULL, NULL);
+    VALUES(697, 9, '2016-06-18 15:00', 24, 90, '2016-06-18 15:00', NULL);
 
+/*
 --Mo 	20.06. 21:00 	Saint Étienne 	B 	Slowakei 	 -  	England
 --Mo 	20.06. 21:00 	Toulouse 	B 	Russland 	 -  	Wales
+*/
 INSERT INTO bo_gamelist(id, bo_index, bo_date, bo_season_ref, bo_group_ref , bo_datetime, bo_openligaid)
-    VALUES(698, 10, '2016-06-19 21:00', 24, 91, NULL, NULL);
+    VALUES(698, 10, '2016-06-19 21:00', 24, 91, '2016-06-19 21:00', NULL);
 
+/*
 --Di 	21.06. 18:00 	Marseille 	C 	Ukraine 	 -  	Polen
 --Di 	21.06. 18:00 	Paris 	C 	Nordirland 	 -  	Deutschland
 --Di 	21.06. 21:00 	Bordeaux 	D 	Kroatien 	 -  	Spanien
 --Di 	21.06. 21:00 	Lens 	D 	Tschechien 	 -  	Türkei
+*/
 INSERT INTO bo_gamelist(id, bo_index, bo_date, bo_season_ref, bo_group_ref , bo_datetime, bo_openligaid)
-    VALUES(699, 11, '2016-06-17 18:00', 24, 92, NULL, NULL);
+    VALUES(699, 11, '2016-06-17 18:00', 24, 92, '2016-06-17 18:00', NULL);
 
+/*
 --Mi 	22.06. 18:00 	Lyon 	F 	Ungarn 	 -  	Portugal
 --Mi 	22.06. 18:00 	Paris St. Denis 	F 	Island 	 -  	Österreich
 --Mi 	22.06. 21:00 	Lille 	E 	Italien 	 -  	Irland
 --Mi 	22.06. 21:00 	Nizza 	E 	Schweden 	 -  	Belgien
+*/
 INSERT INTO bo_gamelist(id, bo_index, bo_date, bo_season_ref, bo_group_ref , bo_datetime, bo_openligaid)
-    VALUES(700, 12, '2016-06-18 18:00', 24, 95, NULL, NULL);
+    VALUES(700, 12, '2016-06-18 18:00', 24, 95, '2016-06-18 18:00', NULL);
+
+COMMIT;
