874033431c81e19a31a96e692b367ea3bf80a231
Andre Winkler <andre.winkler@web.de>
2016-11-06 13:10:53 +0100

    UPDATE: Set a default value

--- src/database/update-2.3.0/update_bo_user_season.sql
+++ src/database/update-2.3.0/update_bo_user_season.sql
@@ -1,7 +1,8 @@
-select 'Start upgrade of betoffice 2.2.0 to 2.3.0 MySQL/MariaDB schema.' as INFO;
-select version();
+SELECT 'Start upgrade of betoffice 2.2.0 to 2.3.0 MySQL/MariaDB schema.' as INFO;
+SELECT version();
 
 ALTER TABLE bo_user_season ADD bo_roletype INTEGER;
 ALTER TABLE bo_user ADD bo_admin BIT;
 
--- TODO Set default value !!!
+UPDATE bo_user SET bo_admin = 0;
+COMMIT;
