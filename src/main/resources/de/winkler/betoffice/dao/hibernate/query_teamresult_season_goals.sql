e815bff05f4d6549d83d94d8727480cadddec599
Andre Winkler <andre.winkler@web.de>
2017-11-07 19:02:15 +0100

    UPDATE: Added shortname and xshortname as new properties to team.

--- src/main/resources/de/winkler/betoffice/dao/hibernate/query_teamresult_season_goals.sql
+++ src/main/resources/de/winkler/betoffice/dao/hibernate/query_teamresult_season_goals.sql
@@ -1,12 +1,27 @@
 select
-  tabelle.id, tabelle.bo_name, tabelle.bo_longname, tabelle.bo_logo,
-  tabelle.bo_teamtype, tabelle.bo_location_ref, tabelle.bo_openligaid,
+  tabelle.id,
+  tabelle.bo_name,
+  tabelle.bo_longname,
+  tabelle.bo_shortname,
+  tabelle.bo_xshortname,
+  tabelle.bo_logo,
+  tabelle.bo_teamtype,
+  tabelle.bo_location_ref,
+  tabelle.bo_openligaid,
   SUM(pos_goals - neg_goals) as diff, SUM(pos_goals) as pos_goals, SUM(neg_goals) as neg_goals
 from
 (
     (
         select
-          t.id, t.bo_name, t.bo_longname, t.bo_logo, t.bo_teamtype, t.bo_location_ref, t.bo_openligaid,
+          t.id,
+          t.bo_name,
+          t.bo_longname,
+          t.bo_shortname,
+          t.bo_xshortname,
+          t.bo_logo,
+          t.bo_teamtype,
+          t.bo_location_ref,
+          t.bo_openligaid,
           SUM(m.bo_homegoals) as pos_goals, SUM(m.bo_guestgoals) as neg_goals
         from
           bo_season s,
@@ -33,7 +48,15 @@ from
     UNION ALL
     (
         select
-           t.id, t.bo_name, t.bo_longname, t.bo_logo, t.bo_teamtype, t.bo_location_ref, t.bo_openligaid,
+           t.id,
+           t.bo_name,
+           t.bo_longname,
+           t.bo_shortname,
+           t.bo_xshortname,
+           t.bo_logo,
+           t.bo_teamtype,
+           t.bo_location_ref,
+           t.bo_openligaid,
            SUM(m.bo_guestgoals) as pos_goals, SUM(m.bo_homegoals) as neg_goals
         from
           bo_season s,
