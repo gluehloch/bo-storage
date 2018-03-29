5bac5e7159146301c2c5a182793a60fa9fe96c08
Andre Winkler <andre.winkler@web.de>
2018-03-29 06:45:16 +0200

    UPDATE: Reactivate the incredible Groovy maintenance script.

--- src/script/maintenance.groovy
+++ src/script/maintenance.groovy
@@ -1,6 +1,7 @@
 @Grab(group='org.slf4j', module='slf4j-api', version='1.6.1')
 @Grab(group='commons-pool', module='commons-pool', version='1.5.4')
 @Grab(group='commons-dbcp', module='commons-dbcp', version='1.4')
+@Grab(group='mysql', module='mysql-connector-java', version='5.1.31')
 @Grab(group='xml-apis', module='xml-apis', version='1.0.b2')
 @Grab(group='de.winkler.betoffice', module='betoffice-storage', version='2.5.1')
 
@@ -26,8 +27,13 @@ print liga.name
 try {
 	// def mysql = new MySqlDatabasedTestSupport()
 	def context = new ClassPathXmlApplicationContext(
-	    ['classpath:/betoffice-persistence.xml', 'classpath:/betoffice-datasource.xml', 'file:/hibernate-mysql-test.xml'] as String[])
+	    ['classpath:/betoffice-persistence.xml', 'classpath:/betoffice-datasource.xml', 'file:hibernate.xml'] as String[])
 	def maintenanceService = context.getBean('databaseMaintenanceService')
+	def seasonService = context.getBean('seasonManagerService')
+
+	def seasonOptional = seasonService.findSeasonByName('Fussball Bundesliga', '2016/2017')
+	def season = seasonOptional.get()
+	print season.name + " - " + season.year
 } catch (Exception ex) {
     ex.printStackTrace()
 }
