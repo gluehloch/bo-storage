eaffba705220909bce947857464ad4887d621d11
andrewinkler <andre.winkler@web.de>
2018-03-30 07:34:40 +0200

    Under construction: A Groovy script to create the season 'WM Russland'

--- src/script/maintenance.groovy
+++ src/script/maintenance.groovy
@@ -7,27 +7,9 @@
 @Grab(group='de.awtools', module='awtools-basic', version='1.4.0-SNAPSHOT')
 @Grab(group='de.winkler.betoffice', module='betoffice-storage', version='2.5.1')
 
-import org.springframework.context.ApplicationContext
 import org.springframework.context.support.ClassPathXmlApplicationContext
-import org.springframework.beans.factory.BeanFactory
-
-import de.winkler.betoffice.BetofficeStore
-
-// import de.winkler.betoffice.test.database.MySqlDatabasedTestSupport
-// import de.winkler.betoffice.test.database.MySqlDatabasedTestSupport.DataLoader
-
-/*
-BetofficeStore betofficeStore = new BetofficeStore();
-betofficeStore.main(new String[0]);
-def service = main.seasonManagerService
-
-// findSeasonByName(String name, String year);
-def liga = service.find('Bundesliga', '2016/2017');
-print liga.name
-*/
 
 try {
-	// def mysql = new MySqlDatabasedTestSupport()
 	def context = new ClassPathXmlApplicationContext(
 	    ['classpath:/betoffice-persistence.xml', 'classpath:/betoffice-datasource.xml', 'file:hibernate.xml'] as String[])
 	def maintenanceService = context.getBean('databaseMaintenanceService')
