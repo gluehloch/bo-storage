8ce3632fcfd93f6546115188a4e8f87cfe4d9dad
Andre Winkler <andre.winkler@web.de>
2018-04-04 18:26:52 +0200

    Under construction: Selecting user points from a ko game.

--- src/database/update-2.6.0/update_bo_game.sql
+++ src/database/update-2.6.0/update_bo_game.sql
@@ -1,4 +1,4 @@
 SELECT 'Start upgrade of betoffice 2.5.0 to 2.6.0 MySQL/MariaDB schema.' as INFO;
 SELECT version();
 
-ALTER TABLE bo_game ADD bo_ko bit;
+ALTER TABLE bo_game ADD bo_ko tinyint(1) NOT NULL DEFAULT 0;
--- src/main/resources/de/winkler/betoffice/dao/hibernate/query_userresult_season_10.sql
+++ src/main/resources/de/winkler/betoffice/dao/hibernate/query_userresult_season_10.sql
@@ -1,52 +1,82 @@
-select
-  count(*) as 'half_points', u.*
-from
-  bo_gamelist gl,
-  bo_game m,
-  bo_gametipp t,
-  bo_season s,
-  bo_user_season us,
-  bo_user u
-where
-      gl.bo_season_ref = :season_id
-  and s.id = :season_id
-  and us.bo_season_ref = s.id
-  and us.bo_user_ref = u.id
-  and m.bo_gamelist_ref = gl.id
-  and m.bo_isplayed = 1
-  and t.bo_game_ref = m.id
-  and t.bo_status <> 0 
-  and t.bo_user_ref = u.id
-  /* Alle Tipps die 1.3 Punkte Tipps sind ausschliessen */
-  and not
+SELECT
+  COUNT(*) AS 'half_points', u.*
+FROM
+  bo_gamelist gl
+  JOIN bo_game m ON (m.bo_gamelist_ref = gl.id)
+  JOIN
+  (
+    SELECT
+      mt.id,
+      CASE
+        WHEN bo_ko = 0 THEN
+          mt.bo_homegoals
+        -- Ende nach 90 Minuten
+        WHEN bo_ko = 1 AND mt.bo_homegoals <> mt.bo_guestgoals THEN
+          mt.bo_homegoals
+        -- Ende nach der Verlaengerung
+        WHEN bo_ko = 1 AND mt.bo_homegoals = mt.bo_guestgoals AND mt.bo_overtimehomegoals <> mt.bo_overtimeguestgoals THEN
+          mt.bo_overtimehomegoals
+        ELSE
+          mt.bo_penaltyhomegoals
+      END as bo_homegoals,
+      CASE
+        WHEN bo_ko = 0 THEN
+          mt.bo_guestgoals
+        -- Ende nach 90 Minuten
+        WHEN bo_ko = 1 AND mt.bo_homegoals <> mt.bo_guestgoals THEN
+          mt.bo_guestgoals
+        -- Ende nach der Verlaengerung
+        WHEN bo_ko = 1 AND mt.bo_homegoals = mt.bo_guestgoals AND mt.bo_overtimehomegoals <> mt.bo_overtimeguestgoals THEN
+          mt.bo_overtimeguestgoals
+        ELSE
+          mt.bo_penaltyguestgoals
+      END as bo_guestgoals
+    FROM
+      bo_game mt
+      /*
+    WHERE
+      mt.bo_gamelist_ref = gl.id
+      */
+  ) as gameresult ON (gameresult.id = m.id)
+  JOIN bo_gametipp t ON (t.bo_game_ref = m.id)
+  JOIN bo_season s ON (gl.bo_season_ref = s.id)
+  JOIN bo_user_season us ON (us.bo_season_ref = s.id)
+  JOIN bo_user u ON (t.bo_user_ref = u.id)
+WHERE
+      s.id = 26 /*:season_id*/
+  AND us.bo_user_ref = u.id
+  AND m.bo_isplayed = 1
+  AND t.bo_status <> 0
+  /* Alle 13 Punkte Tipps ausschliessen. */
+  AND NOT
   (
         t.bo_homegoals  = m.bo_homegoals
-    and t.bo_guestgoals = m.bo_guestgoals
+    AND t.bo_guestgoals = m.bo_guestgoals
   )
-  and
+  AND
   (
     (
       /* Toto 1 */
-          m.bo_homegoals > m.bo_guestgoals
+          gameresult.bo_homegoals > gameresult.bo_guestgoals
       /* User Toto 1*/
-      and t.bo_homegoals > t.bo_guestgoals
+      AND t.bo_homegoals > t.bo_guestgoals
     )
-    or
+    OR
     (
       /* Toto 0 */
-          m.bo_homegoals = m.bo_guestgoals
+          gameresult.bo_homegoals = gameresult.bo_guestgoals
       /* User Toto 0 */
-      and t.bo_homegoals = t.bo_guestgoals
+      AND t.bo_homegoals = t.bo_guestgoals
     )
-    or
+    OR
     (
       /* Toto 2 */
-          m.bo_homegoals < m.bo_guestgoals
+          gameresult.bo_homegoals < gameresult.bo_guestgoals
       /* User Toto 2 */
-      and t.bo_homegoals < t.bo_guestgoals
+      AND t.bo_homegoals < t.bo_guestgoals
     )
   )
-group by
+GROUP BY
   u.bo_nickname
-order by
-  count(*) desc;
+ORDER BY
+  COUNT(*) DESC;
