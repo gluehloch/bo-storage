4c29e3bf059adc31bb85fc952d07a57d0dce2f36
Andre Winkler <andre.winkler@web.de>
2019-06-22 14:44:04 +0200

    UPDATE: Starting the community project.

---
+++ src/main/java/de/winkler/betoffice/storage/Community.java
@@ -0,0 +1,99 @@
+/*
+ * ============================================================================
+ * Project betoffice-storage Copyright (c) 2000-2019 by Andre Winkler. All
+ * rights reserved.
+ * ============================================================================
+ * GNU GENERAL PUBLIC LICENSE TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND
+ * MODIFICATION
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the License, or (at your option) any later
+ * version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc., 59 Temple
+ * Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ */
+
+package de.winkler.betoffice.storage;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.OrderColumn;
+import javax.persistence.Table;
+
+/**
+ * Defines a group of players.
+ *
+ * @author Andre Winkler
+ */
+@Entity
+@Table(name = "bo_community")
+public class Community {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", updatable = false, nullable = false)
+    private Long id;
+    
+    @Column(name = "name")
+    private String name;
+    
+    @ManyToOne
+    @JoinColumn(name = "bo_user_ref")
+    private User communityManager;
+    
+    @OneToMany(mappedBy = "users", fetch = FetchType.LAZY)
+    @OrderColumn(name = "bo_index")
+    private Set<User> users = new HashSet<>();
+  
+    public Long getId() {
+        return id;
+    }
+    
+    protected void setId(Long id) {
+        this.id = id;
+    }
+    
+    public String getName() {
+        return name;
+    }
+    
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    public User getCommunityManager() {
+        return communityManager;
+    }
+    
+    public void setCommunityManager(User user) {
+        this.communityManager = user;
+    }
+    
+    public Set<User> getUsers() {
+        return users;
+    }
+    
+    public void setUsers(Set<User> users) {
+        this.users = users;
+    }
+    
+}
