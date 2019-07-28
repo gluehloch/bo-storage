a977e98cec2b1e871ae3a77aceec214fdf367dc3
Andre Winkler <andre.winkler@web.de>
2019-07-28 09:06:09 +0200

    Changed version number: 2.7.0-SNAPSHOT

---
+++ src/database/update-2.7.0/update_bo_community.sql
@@ -0,0 +1,27 @@
+SELECT 'Start upgrade of betoffice 2.6.0 to 2.7.0 MariaDB schema.' as INFO;
+SELECT version();
+
+--
+-- Community Edition
+--
+create table bo_community (
+    id BIGINT NOT NULL auto_increment,
+    bo_name VARCHAR(40),
+    bo_user_ref BIGINT NOT NULL,
+    primary key (id)
+) ENGINE=InnoDB;
+
+create table bo_community_user (
+    id BIGINT NOT NULL auto_increment,
+    bo_community_ref BIGINT NOT NULL,
+    bo_user_ref BIGINT NOT NULL,
+    primary key (id)
+) ENGINE=InnoDB;
+
+alter table bo_community_user
+    add index idx_community_user_user(bo_user_ref),
+    add constraint fk_community_user_user foreign key (bo_user_ref) references bo_user(id);
+
+alter table bo_community_user
+    add index idx_community_user_community(bo_community_ref),
+    add constraint fk_community_user_community foreign key (bo_community_ref) references bo_community(id);
