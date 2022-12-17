18c6ff5d11bd39c8948e93028732ef14773b5421
Andre Winkler <gluehloch@googlemail.com>
2022-12-17 17:43:29 +0100

    Community Edition: SQL.

--- betoffice.session.sql
+++ betoffice.session.sql
@@ -1,3 +1,6 @@
+select * from bo_community;
+
+
 SELECT
     g.id, gt.bo_user_ref, gt.id
 FROM
@@ -11,4 +14,56 @@ GROUP BY
     g.id, gt.bo_user_ref HAVING COUNT(*) > 1
 ORDER BY
     gt.id
+;
 
+SELECT
+  COUNT(*) AS 'full_points', u.*
+FROM
+  bo_gamelist gl
+  JOIN bo_game m ON (m.bo_gamelist_ref = gl.id)
+  JOIN
+  (
+    SELECT
+      mt.id,
+      CASE
+        WHEN bo_ko = 0 THEN
+          mt.bo_homegoals
+        -- Ende nach 90 Minuten
+        WHEN bo_ko = 1 AND mt.bo_homegoals <> mt.bo_guestgoals THEN
+          mt.bo_homegoals
+        -- Ende nach der Verlaengerung
+        WHEN bo_ko = 1 AND mt.bo_homegoals = mt.bo_guestgoals AND mt.bo_overtimehomegoals <> mt.bo_overtimeguestgoals THEN
+          mt.bo_overtimehomegoals
+        ELSE
+          mt.bo_penaltyhomegoals
+      END as bo_homegoals,
+      CASE
+        WHEN bo_ko = 0 THEN
+          mt.bo_guestgoals
+        -- Ende nach 90 Minuten
+        WHEN bo_ko = 1 AND mt.bo_homegoals <> mt.bo_guestgoals THEN
+          mt.bo_guestgoals
+        -- Ende nach der Verlaengerung
+        WHEN bo_ko = 1 AND mt.bo_homegoals = mt.bo_guestgoals AND mt.bo_overtimehomegoals <> mt.bo_overtimeguestgoals THEN
+          mt.bo_overtimeguestgoals
+        ELSE
+          mt.bo_penaltyguestgoals
+      END as bo_guestgoals
+    FROM
+      bo_game mt
+  ) as gameresult ON (gameresult.id = m.id)
+  JOIN bo_gametipp t ON (t.bo_game_ref = m.id)
+  JOIN bo_season s ON (gl.bo_season_ref = s.id)
+  JOIN bo_community cm ON (cm.bo_season_ref = s.id)
+  JOIN bo_community_user cm_user ON (cm_user.bo_community_ref = cm.id)
+  JOIN bo_user u ON (cm_user.bo_user_ref = u.id AND cm_user.bo_user_ref = u.id)
+WHERE
+      cm.id = :community_id
+  AND m.bo_isplayed = 1
+  AND t.bo_status <> 0
+  AND t.bo_homegoals = gameresult.bo_homegoals
+  AND t.bo_guestgoals = gameresult.bo_guestgoals
+GROUP BY
+  u.bo_nickname
+ORDER BY
+  COUNT(*) DESC;
