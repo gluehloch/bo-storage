ffe4e4cc456b2f01038386ca848121f37e4dd7c5
Andre Winkler <gluehloch@googlemail.com>
2024-01-20 10:49:02 +0100

    Code format.

--- src/main/java/de/winkler/betoffice/service/TippService.java
+++ src/main/java/de/winkler/betoffice/service/TippService.java
@@ -1,6 +1,6 @@
 /*
  * ============================================================================
- * Project betoffice-storage Copyright (c) 2000-2023 by Andre Winkler. All
+ * Project betoffice-storage Copyright (c) 2000-2024 by Andre Winkler. All
  * rights reserved.
  * ============================================================================
  * GNU GENERAL  LICENSE TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND
@@ -37,8 +37,7 @@ import de.winkler.betoffice.storage.UserResultOfDay;
 import de.winkler.betoffice.storage.enums.TippStatusType;
 
 /**
- * Allocates a service for adding, updating and removing tipps. A special method
- * for evaluating Tipp mails.
+ * Allocates a service for adding, updating and removing tipps. A special method for evaluating Tipp mails.
  *
  * @author by Andre Winkler
  */
@@ -47,91 +46,72 @@ public interface TippService {
     /**
      * Berechnet die Spieltagspunkte fuer einen Teilnehmer.
      * 
-     * @param round
-     *            Die Runde, die berechnet werden soll.
-     * @param user
-     *            Der Teilnehmer, dessen Punktestand ermittelt werden soll.
-     * @return Der errechnete Punktestand.
+     * @param  round Die Runde, die berechnet werden soll.
+     * @param  user  Der Teilnehmer, dessen Punktestand ermittelt werden soll.
+     * @return       Der errechnete Punktestand.
      */
     UserResultOfDay getUserPoints(GameList round, User user);
 
     /**
-     * Einen Tipp einer Spielpaarung hinzufügen/aktualisieren.
-     * <b>ACHTUNG:</b> Es findet keine Prüfung statt, ob der Tipp vor Spielbeginn abgegeben wurde.
+     * Einen Tipp einer Spielpaarung hinzufügen/aktualisieren. <b>ACHTUNG:</b> Es findet keine Prüfung statt, ob der
+     * Tipp vor Spielbeginn abgegeben wurde.
      *
-     * @param token
-     *            Das Anmeldetoken mit dem dieser Tipp angelegt wird.
-     * @param match
-     *            Die betreffende Partie.
-     * @param user
-     *            Der Tipper.
-     * @param tipp
-     *            Das getippte Endergebnis
-     * @param status
-     *            Tipp-Status.
-     * @return Der erstellte {@link GameTipp}.
+     * @param  token  Das Anmeldetoken mit dem dieser Tipp angelegt wird.
+     * @param  match  Die betreffende Partie.
+     * @param  user   Der Tipper.
+     * @param  tipp   Das getippte Endergebnis
+     * @param  status Tipp-Status.
+     * @return        Der erstellte {@link GameTipp}.
      */
     GameTipp createOrUpdateTipp(String token, Game match, User user, GameResult tipp, TippStatusType status);
 
     /**
-     * Legt die Tipps für einen kompletten Spieltag in der Datenbank an.
-     * <b>ACHTUNG:</b> Es findet keine Prüfung statt, ob der Tipp vor Spielbeginn abgegeben wurde.
+     * Legt die Tipps für einen kompletten Spieltag in der Datenbank an. <b>ACHTUNG:</b> Es findet keine Prüfung statt,
+     * ob der Tipp vor Spielbeginn abgegeben wurde.
      *
-     * @param token
-     *            Das Anmeldetoken mit dem dieser Tipp angelegt wird.
-     * @param round
-     *            Der betreffende Spieltag.
-     * @param user
-     *            Der Tipper.
-     * @param tipps
-     *            Die Tipps für alle Spiele des Spieltags.
-     * @param status
-     *            Der Status für diese Tipps.
-     * @return Die erstellen {@code GameTipps}.
+     * @param  token  Das Anmeldetoken mit dem dieser Tipp angelegt wird.
+     * @param  round  Der betreffende Spieltag.
+     * @param  user   Der Tipper.
+     * @param  tipps  Die Tipps für alle Spiele des Spieltags.
+     * @param  status Der Status für diese Tipps.
+     * @return        Die erstellen {@code GameTipps}.
      */
-    List<GameTipp> createOrUpdateTipp(String token, GameList round, User user, List<GameResult> tipps, TippStatusType status);
+    List<GameTipp> createOrUpdateTipp(String token, GameList round, User user, List<GameResult> tipps,
+            TippStatusType status);
 
     /**
      * Legt den Tipp für einen Spieler für einen kompletten Spieltag an.
      * 
      * <b>ACHTUNG:</b> Es findet eine PRÜFUNG statt, ob der Tipp VOR Spielbeginn abgegeben wurde.
      * 
-     * @param tippDto
-     *            Der Spieltipp
-     * @return Die erstellen {@code GameTipps}.
+     * @param  tippDto Der Spieltipp
+     * @return         Die erstellen {@code GameTipps}.
      */
     List<GameTipp> validateKickOffTimeAndAddTipp(TippDto tippDto);
 
     /**
      * Liefert alle Spieltipps zu einer Spielpaarung.
      *
-     * @param match
-     *            Die Spielpaarung deren Spieltipps gesucht werden.
-     * @return Die Spieltipps.
+     * @param  match Die Spielpaarung deren Spieltipps gesucht werden.
+     * @return       Die Spieltipps.
      */
     List<GameTipp> findTipps(Game match);
 
     /**
      * Liefert den Spieltipp einer Spielers zu einer Spielpaarung.
      * 
-     * @param game
-     *            Das Spiel
-     * @param user
-     *            Der Teilnehmer
-     * @return Der Spieltipp zu den gesuchten Parametern
+     * @param  game Das Spiel
+     * @param  user Der Teilnehmer
+     * @return      Der Spieltipp zu den gesuchten Parametern
      */
     Optional<GameTipp> findTipp(Game game, User user);
 
     /**
      * Liefert alle Spieltipps zu einem Spieltag von einem Teilnehmer.
      *
-     * @param round
-     *            Der Spieltag der für die Suche herangezogen werden soll.
-     * 
-     * @param user
-     *            Die Spieltipps von diesem User suchen.
-     * 
-     * @return Die Spieltipps.
+     * @param  round Der Spieltag der für die Suche herangezogen werden soll.
+     * @param  user  Die Spieltipps von diesem User suchen.
+     * @return       Die Spieltipps.
      */
     default List<GameTipp> findTipps(GameList round, User user) {
         return findTipps(round.getId(), user.getId());
@@ -140,44 +120,37 @@ public interface TippService {
     /**
      * Liefert alle Spieltipps zu einem Spieltag von einem Teilnehmer.
      *
-     * @param roundId
-     *            Der Spieltag der für die Suche herangezogen werden soll.
+     * @param  roundId Der Spieltag der für die Suche herangezogen werden soll.
      * 
-     * @param userId
-     *            Die Spieltipps von diesem User suchen.
+     * @param  userId  Die Spieltipps von diesem User suchen.
      * 
-     * @return Die Spieltipps.
+     * @return         Die Spieltipps.
      */
     List<GameTipp> findTipps(long roundId, long userId);
 
     /**
      * Liefert alle Spieltipps zu einem Spieltag.
      * 
-     * @param roundId
-     *            Der gesuchte Spieltag.
-     * @return Die Spieltipps.
+     * @param  roundId Der gesuchte Spieltag.
+     * @return         Die Spieltipps.
      */
     List<GameTipp> findTipps(long roundId);
 
     /**
      * Ermittelt den naechsten zu tippenden Spieltag.
      *
-     * @param seasonId
-     *            Die Bezugsmeisterschaft
-     * @param date
-     *            Das Bezugsdatum
-     * @return Der naechste zu tippende Spieltag
+     * @param  seasonId Die Bezugsmeisterschaft
+     * @param  date     Das Bezugsdatum
+     * @return          Der naechste zu tippende Spieltag
      */
     Optional<GameList> findNextTippRound(long seasonId, ZonedDateTime date);
 
     /**
      * Ermittelt den letzten zu tippenden Spieltag.
      *
-     * @param seasonId
-     *            Die Bezugsmeisterschaft
-     * @param date
-     *            Das Bezugsdatum
-     * @return Der naechste zu tippende Spieltag
+     * @param  seasonId Die Bezugsmeisterschaft
+     * @param  date     Das Bezugsdatum
+     * @return          Der naechste zu tippende Spieltag
      */
     Optional<GameList> findPreviousTippRound(long seasonId, ZonedDateTime date);
 
