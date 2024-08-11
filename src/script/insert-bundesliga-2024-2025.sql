f70e39d73bb21cb9a176004b497e9c9e57623370
Andre Winkler <gluehloch@googlemail.com>
2024-08-11 17:04:59 +0200

    SQL script: prepare Bundesliga 2024/2025

--- src/script/insert-bundesliga-2024-2025.sql
+++ src/script/insert-bundesliga-2024-2025.sql
@@ -1,4 +1,4 @@
 insert into bo_community(bo_shortname, bo_name, bo_year, bo_user_ref, bo_season_ref) values ('TDKB 2024/2025', 'Bundesliga', '2024/2025', 6, 37);
 
-insert into bo_community_user(bo_community_ref, bo_user_ref) values
+insert into bo_community_user(bo_community_ref, bo_user_ref)
 select 37, u2.bo_user_ref from bo_community_user u2 where u2.bo_community_ref = 35;
