bd99e837053d01f511c3f1cfacf9b3220c53c657
Andre Winkler <gluehloch@googlemail.com>
2025-02-25 08:09:18 +0100

    WIP: mail change...

--- src/database/update-3.0.1/update_bo_grouptype.sql
+++ src/database/update-3.0.1/update_bo_grouptype.sql
@@ -7,3 +7,9 @@ SELECT 'Start upgrade of betoffice 3.0.1 to 3.0.2 MariaDB schema.' as INFO;
 SELECT version();
 
 ALTER TABLE bo_user ADD bo_notification INT(2) NOT NULL DEFAULT 0;
+
+SELECT 'Start upgrade of betoffice 3.0.1 to 3.0.2 MariaDB schema.' as INFO;
+SELECT version();
+
+ALTER TABLE bo_user ADD bo_change_token VARCHAR(2048) comment 'Change Token';
+ALTER TABLE bo_user ADD bo_change_email VARCHAR(255) comment 'Holds the new email addresse';
