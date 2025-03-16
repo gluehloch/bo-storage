13cbc46c677833156e663251253ac4147e307b4f
Andre Winkler <gluehloch@googlemail.com>
2025-03-16 17:33:37 +0100

    FIX: update sql script.

--- src/database/update-3.0.1/update_bo_grouptype.sql
+++ src/database/update-3.0.1/update_bo_grouptype.sql
@@ -12,4 +12,5 @@ SELECT 'Start upgrade of betoffice 3.0.1 to 3.0.2 MariaDB schema.' as INFO;
 SELECT version();
 
 ALTER TABLE bo_user ADD bo_change_token VARCHAR(2048) comment 'Change Token';
-ALTER TABLE bo_user ADD bo_change_email VARCHAR(255) comment 'Holds the new email addresse';
+ALTER TABLE bo_user ADD bo_change_email VARCHAR(255)  comment 'Holds the new email addresse';
+ALTER TABLE bo_user ADD bo_change_send  INTEGER       comment 'Number of notification mails';
