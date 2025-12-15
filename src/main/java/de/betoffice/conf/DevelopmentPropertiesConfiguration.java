ff3b45aecd2f6e544b13ece0e721d4e4f15e7db2
gluehloch <gluehloch@googlemail.com>
2025-12-15 09:40:20 +0100

    Profile default removed.

--- src/main/java/de/betoffice/conf/DevelopmentPropertiesConfiguration.java
+++ src/main/java/de/betoffice/conf/DevelopmentPropertiesConfiguration.java
@@ -31,7 +31,7 @@ import org.springframework.context.annotation.PropertySource;
 /**
  * Configuration for dev environment.
  */
-@Profile(value = { "dev", "default" })
+@Profile(value = "dev")
 @Configuration
 @PropertySource(ignoreResourceNotFound = true, value = { "classpath:/bodev.properties" })
 public class DevelopmentPropertiesConfiguration extends AbstractPropertiesConfiguration {
