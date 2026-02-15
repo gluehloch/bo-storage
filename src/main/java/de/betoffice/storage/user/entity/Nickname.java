3177bca63f11ad0908937bced4143cbc18ee9f6b
Andre Winkler <gluehloch@googlemail.com>
2026-02-15 09:15:47 +0100

    2000 to 2026

--- src/main/java/de/betoffice/storage/user/entity/Nickname.java
+++ src/main/java/de/betoffice/storage/user/entity/Nickname.java
@@ -1,6 +1,6 @@
 /*
  * ============================================================================
- * Project betoffice-storage Copyright (c) 2000-2022 by Andre Winkler. All
+ * Project betoffice-storage Copyright (c) 2000-2026 by Andre Winkler. All
  * rights reserved.
  * ============================================================================
  * GNU GENERAL PUBLIC LICENSE TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND
