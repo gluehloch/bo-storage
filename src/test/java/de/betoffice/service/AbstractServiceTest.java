076696bd3535a90cc52859701621742828fd8e56
Andre Winkler <gluehloch@googlemail.com>
2026-03-09 07:02:21 +0100

    Problems with Spring 7 and JUnit 6.

--- src/test/java/de/betoffice/service/AbstractServiceTest.java
+++ src/test/java/de/betoffice/service/AbstractServiceTest.java
@@ -8,15 +8,17 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import de.betoffice.conf.PersistenceJPAConfiguration;
 import de.betoffice.conf.TestPropertiesConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
 
 /**
  * Abstract helper class to init the spring test configuration.
  * 
  * @author Andre Winkler
  */
+@SpringJUnitConfig(classes = { PersistenceJPAConfiguration.class, TestPropertiesConfiguration.class })
 @ActiveProfiles(profiles = "test")
-@ExtendWith(SpringExtension.class)
-@ContextConfiguration(classes = { PersistenceJPAConfiguration.class, TestPropertiesConfiguration.class })
+// @ExtendWith(SpringExtension.class)
+// @ContextConfiguration(classes = { PersistenceJPAConfiguration.class, TestPropertiesConfiguration.class })
 @ComponentScan({"de.betoffice", "de.betoffice"})
 public abstract class AbstractServiceTest {
 
