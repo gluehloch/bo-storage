bdc1c1220b6301163ec2d5bbc6b5151be677c2de
andre <gluehloch@googlemail.com>
2026-03-29 17:48:10 +0200

    ADD: SpringJUnitConfig

--- src/test/java/de/betoffice/conf/BetofficeTestConfig.java
+++ src/test/java/de/betoffice/conf/BetofficeTestConfig.java
@@ -28,16 +28,15 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
 
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
+@SpringJUnitConfig(classes = { PersistenceJPAConfiguration.class, TestPropertiesConfiguration.class })
 @ActiveProfiles(profiles = "test")
-@ExtendWith(SpringExtension.class)
-@ContextConfiguration(classes = { PersistenceJPAConfiguration.class, TestPropertiesConfiguration.class })
+@ComponentScan({"de.betoffice", "de.betoffice"})
 public @interface BetofficeTestConfig {
 
 }
